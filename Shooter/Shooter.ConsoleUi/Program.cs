fe8db9644f871321df8e0aa46941f47634d95981
NC <nika.chkhartishvili7@gmail.com>
2020-05-22 12:52:17 +0400

    First shot log added

--- Shooter/Shooter.ConsoleUi/Program.cs
+++ Shooter/Shooter.ConsoleUi/Program.cs
@@ -7,6 +7,8 @@ namespace Shooter.ConsoleUi
     {
         static void Main(string[] args)
         {
+            //იმ შემთხვევაში თუ ეს დააბრუნებს კენტს მაშინ პირველი ისვრის  და შესაბამისად იგებს Player1, თუ ლუწს მაშინ PLayer2
+            int firstShotLogic = new Random().Next(1,7); 
 
            var player1 = new Player();
            player1.Name = "Rambo";
@@ -16,21 +18,40 @@ namespace Shooter.ConsoleUi
            player2.Name = "Bond";
            player2.BuyWeapon(new Glock47());
 
+            
+
 
             while (player1.IsAlive || player2.IsAlive )
             {
+                if(firstShotLogic%2!=0)
+                {
+                    player1.TakeDamage(player2.Shoot());
+                    Console.WriteLine($"{player2.Name} shoots {player1.Name}");
+                    Console.WriteLine($"{player1.Name} Health is {player1.Health}");
+                }
+                else
+                {
+                    player2.TakeDamage(player1.Shoot());
+                    Console.WriteLine($"{player1.Name} shoots {player2.Name}");
+                    Console.WriteLine($"{player2.Name} Health is {player2.Health}");
+                }
 
-                player1.TakeDamage(player2.Shoot());
-                Console.WriteLine($"{player2.Name} shoots {player1.Name}");
-                Console.WriteLine($"{player1.Name} Health is {player1.Health}");
-
-                if (!player1.IsAlive || !player2.IsAlive)
+                if (!player1.IsAlive)
+                {
+                    Console.WriteLine($"...................{player2.Name} WINS................");
                     break;
+                }
 
-               
-                player2.TakeDamage(player1.Shoot()); 
-                Console.WriteLine($"{player1.Name} shoots {player2.Name}");
-                Console.WriteLine($"{player2.Name} Health is {player2.Health}");
+                if (!player2.IsAlive)
+                {
+                    Console.WriteLine($"...................{player1.Name} WINS................");
+                    break;
+                }
+
+
+
+
+
 
             }
 
